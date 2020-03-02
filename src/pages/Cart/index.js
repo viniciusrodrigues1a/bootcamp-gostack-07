0bda15c99edaa27b77ee54370a1e0aa158777982
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-03-02 00:15:11 -0300

    :sparkles: página dos itens do carrinho

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {
+  MdAddCircleOutline,
+  MdDelete,
+  MdRemoveCircleOutline,
+} from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
+
+export default function Cart() {
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/28/D16-4397-128/D16-4397-128_detalhe1.jpg?ims=280x280"
+                alt="Tênis"
+              />
+            </td>
+            <td>
+              <strong>Tênis muito massa</strong>
+              <span>R$ 129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$ 258,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <span>R$ 1920,28</span>
+        </Total>
+      </footer>
+    </Container>
+  );
+}
