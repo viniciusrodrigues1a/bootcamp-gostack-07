0c9edfd849428cb66bd7ea8815d69264400aad0a
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-03-04 16:50:28 -0300

    :sparkles: criando action para adicionar item ao carrinho

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import formatPrice from '../../utils/format';
 import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default class Home extends Component {
+class Home extends Component {
   state = {
     products: [],
   }
@@ -19,6 +20,15 @@ export default class Home extends Component {
     }));
 
     this.setState({ products: data });
+  }
+
+  handleAddProduct = product => {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    })
   }
 
   render() {
@@ -35,7 +45,10 @@ export default class Home extends Component {
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
 
-            <button type="button">
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
               <div>
                 <MdAddShoppingCart size={16} color="#fff" /> 3
               </div>
@@ -48,3 +61,5 @@ export default class Home extends Component {
     );
   }
 }
+
+export default connect()(Home);
